77975059ed066fe87be4f690e3ffdb56de6eb9a6
ramilian <ramilianjose16@gmail.com>
2022-10-19 08:32:45 -0500

    esta asignado el puerto

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
     this.paths = {
       usuarios: "/api/usuarios",
       auth: "/api/auth",
